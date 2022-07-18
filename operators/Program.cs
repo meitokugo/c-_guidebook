48d8034b5c476cb62b34a926bd4539097849b35e
mingdebaba <51104897+mingdebaba@users.noreply.github.com>
2022-07-18 23:42:55 +0900

    calculator

--- operators/Program.cs
+++ operators/Program.cs
@@ -1,7 +1,16 @@
 ﻿using System;
 class Program 
 { static void Main(string[] args) 
-    { int a = 21; int b = 10; if (a == b) 
+    { 
+
+        int a = 60;            /* 60 = 0011 1100 */            
+        int b = 13;            /* 13 = 0000 1101 */        
+        int c = 0;
+
+
+
+        if (a == b) 
+
         { Console.WriteLine("Line 1 - a 等于 b"); } 
         
         else 
@@ -16,8 +25,26 @@ class Program
         a = 5; b = 20; if (a <= b)
         { Console.WriteLine("Line 4 - a 小于或等于 b"); } 
         if (b >= a) 
-        { Console.WriteLine("Line 5 - b 大于或等于 a"); } 
-    } 
+        { Console.WriteLine("Line 5 - b 大于或等于 a"); }
+        c = a & b;         
+        /* 12 = 0000 1100 */           
+        Console.WriteLine("Line 1 - c 的值是 {0}", c); c = a | b;         
+        /* 61 = 0011 1101 */         
+        Console.WriteLine("Line 2 - c 的值是 {0}", c); c = a ^ b;   
+        /* 49 = 0011 0001 */            
+        Console.WriteLine("Line 3 - c 的值是 {0}", c); c = ~a;          
+        /*-61 = 1100 0011 */           
+        Console.WriteLine("Line 4 - c 的值是 {0}", c); c = a << 2;    
+        /* 240 = 1111 0000 */            
+        Console.WriteLine("Line 5 - c 的值是 {0}", c); c = a >> 2;  
+        /* 15 = 0000 1111 */          
+        Console.WriteLine("Line 6 - c 的值是 {0}", c); 
+        Console.ReadLine();
+
+
+
+
+    }
 }
 
 
