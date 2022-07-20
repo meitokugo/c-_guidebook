70e2e78ec46fd793d5b85fad392cada9dcc2d4ab
mingdebaba <51104897+mingdebaba@users.noreply.github.com>
2022-07-20 23:26:44 +0900

    sbyte

---
+++ char_var/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+System.Text.StringBuilder sb = new System.Text.StringBuilder();
+string s1 = "";
+for(int i = 0; i < 50000; i++)
+{
+    s1 = s1 + "ABCDEFG";
+}
+Console.WriteLine("completed");
+
+for(int i =0; i < 5000; i++)
+{
+    sb.Append("ABCDEFG");
+}
+Console.WriteLine("completed");
