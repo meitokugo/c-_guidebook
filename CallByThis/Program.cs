95a0dc51511c2a822c9939b6011a53230e2e81d8
mingdebaba <wmeadal@live.com>
2022-08-12 23:28:52 +0900

    callThis

---
+++ CallByThis/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Customer obj1 = new();
+Customer obj2 = new("A101", "syuwadaro");
+Customer obj3 = new("B101", "yamadachiro", 28);
+Console.WriteLine(
+    obj1.id + "" + obj1.name + "" + obj1.age);
+Console.WriteLine(
+    obj2.id + "" + obj2.name + "" + obj2.age);
+Console.WriteLine(
+    obj3.id + "" + obj3.name + "" + obj3.age);
+
+Console.WriteLine("Hello, World!");
+class Customer
+{
+    public string id;
+    public string name;
+    public int age;
+    public Customer()
+        :this("-","-",-1)
+    {
+
+    }
+    public Customer(string id, string name)
+    : this(id, name, -1)
+    {
+
+    }
+
+    public Customer(string id, string name, int age)
+    {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+};
+
+
