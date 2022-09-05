42433b582a4ce3ea6f30d75345cdd23ebb79d2cd
mingdebaba <wmeadal@live.com>
2022-09-05 22:52:11 +0900

    framework

---
+++ Csちゃん/CSharpchan.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csちゃん
+{
+    internal class CSharpchan
+    {
+        private RandomResponder _res_random;
+
+        private RepeatResponder _res_repeat;
+
+        private Responder _responder;
+        
+        public string? Name { get; set; }
+
+        public CSharpchan(string name)
+        {
+            Name = name;
+            _res_random = new RandomResponder("Random");
+            _res_repeat = new RepeatResponder("Repeat");
+            _responder = new Responder("Responder");
+        }
+        public string Dialogue(string input)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(0, 10);
+            if(num<6)
+            {
+                _responder = _res_random;
+            }
+            else
+            {
+                _responder = _res_repeat;
+            }
+            return _responder.Response(input);
+        }
+
+        public string GetName()
+        {
+            return _responder.Name;
+        }
+    }
+
+    class Responder
+    {
+        public string Name { get; set; }
+        public Responder(string name)
+        {
+            Name = name;
+        }
+
+        public virtual string Response(String input)
+        {
+            return "";
+        }
+    }
+
+    class RepeatResponder : Responder
+    {
+        public RepeatResponder(string name): base(name)
+        {
+
+        }
+
+        public override string Response(string input)
+        {
+            return String.Format("{0}ってなに",input);
+        }
+    }
+    class RandomResponder : Responder
+    {
+        private string[] _responses =
+        {
+            "めっちゃ寒い天気！",
+            "確かにそうだね",
+            "10円拾った",
+            "じゃあこれ知ってる？",
+            "それね",
+            "それがかわいい"
+        };
+        public RandomResponder(string name) : base(name)
+        {
+
+        }
+        public override string Response(string input)
+        {
+            Random rnd = new();
+            return _responses[rnd.Next(0, _responses.Length)];
+        }
+    }
+}
---
+++ Csちゃん/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Csちゃん
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(1, 13);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(260, 485);
+            this.textBox2.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Csちゃん.Properties.Resources.动漫___251;
+            this.pictureBox1.Location = new System.Drawing.Point(272, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(1, 508);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(595, 29);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(627, 508);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 30);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "話す";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(272, 325);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(500, 180);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.Location = new System.Drawing.Point(3, 503);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(595, 29);
+            this.textBox3.TabIndex = 0;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox4.Location = new System.Drawing.Point(3, 12);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox4.Size = new System.Drawing.Size(260, 485);
+            this.textBox4.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(627, 503);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 30);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "話す";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Csちゃん.Properties.Resources.动漫___251;
+            this.pictureBox2.Location = new System.Drawing.Point(269, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(500, 300);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(269, 315);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(500, 181);
+            this.label2.TabIndex = 4;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(784, 541);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Name = "Form1";
+            this.Text = "C#ちゃん";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox2;
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label1;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button2;
+        private PictureBox pictureBox2;
+        private Label label2;
+    }
+}
