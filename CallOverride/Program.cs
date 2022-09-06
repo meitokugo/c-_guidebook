f6033d9b9dd2145a9b783535572f58d9349d7a9d
mingdebaba <wmeadal@live.com>
2022-09-06 23:40:07 +0900

    override/interface

---
+++ CallOverride/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+superClass[] a = {
+new subClassA(),
+new subClassB(),
+new subClassC()
+};
+Call(a);
+static void Call(params superClass[] args)
+{
+    foreach (superClass o in args)
+    {
+        o.Disp();
+    }
+}
+
+abstract class superClass
+{
+    abstract public void Disp();
+}
+class subClassA: superClass{
+    public override void Disp()
+    {
+        Console.WriteLine("商品名はなに？");
+    }
+}
+class subClassB : superClass
+{
+    public override void Disp()
+    {
+        Console.WriteLine("商品名は分からない");
+    }
+}
+class subClassC : superClass
+{
+    public override void Disp()
+    {
+        Console.WriteLine("商品名がわかる");
+    }
+}
+
---
+++ Interface/Program.cs
@@ -0,0 +1,14 @@
+﻿SampleCls sc = new();
+sc.Show();
+// See https://aka.ms/new-console-template for more information
+interface ISample
+{
+    void Show();
+}
+class SampleCls : ISample
+{
+    public void Show()
+    {
+        Console.WriteLine("SampleClsのShow()メソッドです");
+    }
+}
